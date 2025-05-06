5f61cc793763dd3b9097e5bfe614555b115ac709
Petar Petrov <petizad55@gmail.com>
2025-05-06 22:47:06 +0200

    Basic flask (#1)
    * Misc: requirements.txt and dockerfile setup upgrades
    
    * add flask app
    
    * add automatic versioning through git tags
    
    * incorporate lib_ml now that pulling it from git works consistently
    
    * clean up

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,21 @@
 FROM python:3.12.9-slim
 
+ARG VERSION=unknown
+
+ENV VERSION=$VERSION
+
+RUN apt-get update && apt-get install -y git \ 
+        && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /root
+COPY requirements.txt .
+
+RUN python -m pip install --upgrade pip &&\
+        pip install -r requirements.txt
+
+COPY src src
+
+EXPOSE 8080
+
+ENTRYPOINT [ "python" ]
+CMD [ "src/app.py" ]
---
+++ src/app.py
@@ -0,0 +1,112 @@
+"""
+Flask API for the Restaurant sentiment analysis model(s).
+"""
+
+import os
+import joblib
+from flask import Flask, jsonify, request
+from flasgger import Swagger
+
+from lib_ml import Preprocessor
+
+app = Flask(__name__)
+swagger = Swagger(app)
+preprocessor = Preprocessor()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """
+    Predict whether an SMS is Spam.
+    ---
+    consumes:
+      - application/json
+    parameters:
+        - name: input_data
+          in: body
+          description: message to be classified.
+          required: True
+          schema:
+            type: object
+            required: sms
+            properties:
+                sms:
+                    type: string
+                    example: This is an example of an SMS.
+    responses:
+      200:
+        description: "The result of the classification: 'spam' or 'ham'."
+    """
+    input_data = request.get_json()
+    sms = input_data.get('sms')
+    
+    processed_sms = preprocessor.preprocess(sms) 
+    
+    # Pending access to the models
+    #model = joblib.load('output/model.joblib')
+    #prediction = model.predict(processed_sms)[0]
+    prediction = 'Spam'
+    
+    res = {
+        "result": prediction,
+        "classifier": "decision tree",
+        "sms": sms
+    }
+    print(res)
+    return jsonify(res)
+
+@app.route('/dumbpredict', methods=['POST'])
+def dumb_predict():
+    """
+    Predict whether a given SMS is Spam or Ham (dumb model: always predicts 'ham').
+    ---
+    consumes:
+      - application/json
+    parameters:
+        - name: input_data
+          in: body
+          description: message to be classified.
+          required: True
+          schema:
+            type: object
+            required: sms
+            properties:
+                sms:
+                    type: string
+                    example: This is an example of an SMS.
+    responses:
+      200:
+        description: "The result of the classification: 'spam' or 'ham'."
+    """
+    input_data = request.get_json()
+    sms = input_data.get('sms')
+    
+    return jsonify({
+        "result": "Spam",
+        "classifier": "decision tree",
+        "sms": sms
+    })
+
+@app.route("/version")
+def version():
+    """
+    Show model service version
+    ---
+    consumes:
+      - nothing
+    responses:
+      200:
+        description: "The service version"
+    """
+    version = os.getenv("VERSION", "unknown")
+    
+    version = version.split('-')[0] # git describe --tags returns in the format TAG-COMMIT INFO, but we want just the tag
+    
+    return jsonify({
+      "version": version
+    })
+
+if __name__ == '__main__':
+    # clf = joblib.load('output/model.joblib') TODO Pending access to the models
+
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
