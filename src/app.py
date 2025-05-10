3ca237c1b5c145cdd6344315db52bcefae67d6f9
Petar Petrov <petizad55@gmail.com>
2025-05-10 14:20:24 +0200

    use model-training artifacts

--- src/app.py
+++ src/app.py
@@ -15,98 +15,95 @@ preprocessor = Preprocessor()
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    """
-    Predict whether an SMS is Spam.
-    ---
-    consumes:
-      - application/json
-    parameters:
-        - name: input_data
-          in: body
-          description: message to be classified.
-          required: True
-          schema:
-            type: object
-            required: sms
-            properties:
-                sms:
-                    type: string
-                    example: This is an example of an SMS.
-    responses:
-      200:
-        description: "The result of the classification: 'spam' or 'ham'."
-    """
-    input_data = request.get_json()
-    sms = input_data.get('sms')
-    
-    processed_sms = preprocessor.preprocess(sms) 
-    
-    # Pending access to the models
-    #model = joblib.load('output/model.joblib')
-    #prediction = model.predict(processed_sms)[0]
-    prediction = 'Spam'
-    
-    res = {
-        "result": prediction,
-        "classifier": "decision tree",
-        "sms": sms
-    }
-    print(res)
-    return jsonify(res)
+  """
+  Predict whether an SMS is Spam.
+  ---
+  consumes:
+    - application/json
+  parameters:
+      - name: input_data
+        in: body
+        description: message to be classified.
+        required: True
+        schema:
+          type: object
+          required: sms
+          properties:
+              sms:
+                  type: string
+                  example: This is an example of an SMS.
+  responses:
+    200:
+      description: "The result of the classification: 'spam' or 'ham'."
+  """
+  input_data = request.get_json()
+  sms = input_data.get('sms')
+  
+  vectorizer = joblib.load("output/bow_vectorizer.pkl")
+  model = joblib.load('output/sentiment_model.pkl')
+  
+  processed_sms = preprocessor.preprocess(sms) 
+  vectorized = vectorizer.transform([processed_sms]).toarray()
+  
+  prediction = model.predict(vectorized)[0]
+  prediction = 'Spam'
+  
+  res = {
+      "result": prediction,
+      "sms": sms
+  }
+  print(res)
+  return jsonify(res)
 
 @app.route('/dumbpredict', methods=['POST'])
 def dumb_predict():
-    """
-    Predict whether a given SMS is Spam or Ham (dumb model: always predicts 'ham').
-    ---
-    consumes:
-      - application/json
-    parameters:
-        - name: input_data
-          in: body
-          description: message to be classified.
-          required: True
-          schema:
-            type: object
-            required: sms
-            properties:
-                sms:
-                    type: string
-                    example: This is an example of an SMS.
-    responses:
-      200:
-        description: "The result of the classification: 'spam' or 'ham'."
-    """
-    input_data = request.get_json()
-    sms = input_data.get('sms')
-    
-    return jsonify({
-        "result": "Spam",
-        "classifier": "decision tree",
-        "sms": sms
-    })
+  """
+  Predict whether a given SMS is Spam or Ham (dumb model: always predicts 'ham').
+  ---
+  consumes:
+    - application/json
+  parameters:
+      - name: input_data
+        in: body
+        description: message to be classified.
+        required: True
+        schema:
+          type: object
+          required: sms
+          properties:
+              sms:
+                  type: string
+                  example: This is an example of an SMS.
+  responses:
+    200:
+      description: "The result of the classification: 'spam' or 'ham'."
+  """
+  input_data = request.get_json()
+  sms = input_data.get('sms')
+  
+  return jsonify({
+      "result": "Spam",
+      "classifier": "decision tree",
+      "sms": sms
+  })
 
 @app.route("/version")
 def version():
-    """
-    Show model service version
-    ---
-    consumes:
-      - nothing
-    responses:
-      200:
-        description: "The service version"
-    """
-    version = os.getenv("VERSION", "unknown")
-    
-    version = version.split('-')[0] # git describe --tags returns in the format TAG-COMMIT INFO, but we want just the tag
-    
-    return jsonify({
-      "version": version
-    })
+  """
+  Show model service version
+  ---
+  consumes:
+    - nothing
+  responses:
+    200:
+      description: "The service version"
+  """
+  version = os.getenv("VERSION", "unknown")
+
+  return jsonify({
+    "version": version
+  })
 
 if __name__ == '__main__':
-    # clf = joblib.load('output/model.joblib') TODO Pending access to the models
-
-    app.run(host="0.0.0.0", port=8080, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=True)
     
